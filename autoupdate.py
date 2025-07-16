3808e21450cf1455b707389e0a55fbee0cefecd0
ShenSenlin <15068749075@163.com>
2025-07-16 07:42:17 +0200

    Game Data Update

--- autoupdate.py
+++ autoupdate.py
@@ -4,6 +4,6 @@ import os
 while True:
     os.system("git add .")
     os.system('git commit -m "Game Data Update"')
-    os.sytem('git push origin main')
+    os.system('git push origin main')
 
     time.sleep(300)
